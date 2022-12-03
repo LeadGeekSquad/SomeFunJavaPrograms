219775bba54392b3bdac58d2b4f287e00ef801c2
namanpatel <namanpatel1997@yahoo.com>
2022-12-03 17:44:45 -0600

    Added variables in class Supreme

--- src/com/namanpatel/Supreme.java
+++ src/com/namanpatel/Supreme.java
@@ -7,6 +7,12 @@ pulbic class Supreme {
 		public static void main(String[] args) {
 			
 		//Add code here
+		private String name;
+		private String email;
+		private String phoneNumber;
+		private String streetName;
+		private String state;
+		private String country;
 
   }
 }
