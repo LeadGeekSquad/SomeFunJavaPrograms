42aee0d454741b9e05fba373799e9c67b6200dc1
namanpatel <namanpatel1997@yahoo.com>
2022-12-03 17:01:24 -0600

    Adding class Supreme

---
+++ src/com/namanpatel/Supreme.java
@@ -0,0 +1,12 @@
+package com.namanpatel;
+import java.util.Scanner;
+import java.util.Arrays;
+
+pulbic class Supreme {
+
+		public static void main(String[] args) {
+			
+		//Add code here
+
+  }
+}
